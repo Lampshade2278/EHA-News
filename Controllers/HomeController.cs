b4061976c008d1ae60eed293dc304e5f9aa7c954
E N <noralel053@gmail.com>
2024-11-22 19:51:30 -0600

    Something

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EN_Assignment5.Models;
+using System.Collections.Generic;
+
+namespace Assignment_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Ethan  De La Rosa";
+            ViewBag.Education = "Associates";
+            ViewBag.Birth = "Yokosuka, Japan";
+
+            var holdModel = new Hold
+            {
+                Hobbies = new List<string> { "Video Games", "Poetry", "Reading" },
+                Classes = new List<string> { "Software Engineering", "Computing Ethics", "Operating Systems" }
+            };
+
+            return View(holdModel);
+        }
+    }
+}
---
+++ Models/Hold.cs
@@ -0,0 +1,8 @@
+﻿namespace EN_Assignment5.Models
+{
+    public class Hold
+    {
+        public List<string> Hobbies { get; set; }
+        public List<string> Classes { get; set; }
+    }
+}
