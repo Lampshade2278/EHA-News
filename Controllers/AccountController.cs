d6f49093fc1a9f794955d8f16fbfd4768b1c4130
Hal <hjbieniek2003@gmail.com>
2024-11-25 00:48:42 -0600

    Added Methods to interface with Views

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EHA_News.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EHA_News.Controllers
 {
@@ -6,6 +7,39 @@ namespace EHA_News.Controllers
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Signup()
+        {
+            /* Create new account to put in database */
+            var account = new Account("","");
+            return View(account);
+        }
+
+        [HttpPost]
+        public IActionResult Signup(Account account)
+        {
+            /*Add functions to Check Name and Password requirements, 
+             put account in database, and then Redirect to home */
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            /* Create container to hold credentials during login */
+            var login = new LoginModel();
+            return View(login);
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginModel login)
+        {
+            /*Add functions to Check Credentials and then Redirect to home */
+
             return View();
         }
     }
