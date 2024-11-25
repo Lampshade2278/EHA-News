ab5493e5628f319683f399270e182e7b443b7002
Hal <hjbieniek2003@gmail.com>
2024-11-25 00:50:10 -0600

    Added more Attributes and changed from Private to Public

--- Models/Account.cs
+++ Models/Account.cs
@@ -2,20 +2,28 @@
 {
     public class Account
     {
-        private string AccountID { get; set; }
-        private string Password { get; set; }
+        
+        public string AccountId { get; set; }
+        
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
 
         // Constructor
         public Account(string username, string password)
         {
-            AccountID = username;
+            AccountId = username;
             Password = password;
         }
 
         // Validate user credentials
         public bool Validate(string enteredUsername, string enteredPassword)
         {
-            return AccountID == enteredUsername && Password == enteredPassword;
+            return AccountId == enteredUsername && Password == enteredPassword;
         }
     }
 }
