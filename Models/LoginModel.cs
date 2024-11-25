51f61ea9aa26d537f6469076f9b097c139aca6af
Hal <hjbieniek2003@gmail.com>
2024-11-25 00:51:10 -0600

    Login and Signup View functionality

---
+++ Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EHA_News.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Please enter your Email.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Password.")]
+        public string Password { get; set; }
+    }
+}
