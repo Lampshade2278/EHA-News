a02415a5c972e73e5c278e3afc8a5533289de59c
E N <noralel053@gmail.com>
2024-11-29 22:32:18 -0600

    Push for the article controller and model as well as change to the index

--- Models/Article.cs
+++ Models/Article.cs
@@ -3,11 +3,18 @@
     public class Article
     {
         public string Headline { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+        public string Category { get; set; }
 
-        public Article(string headline)
+        // Constructor with multiple fields
+        public Article(string headline, string content, string author, string category)
         {
             Headline = headline;
+            Content = content;
+            Author = author;
+            Category = category;
         }
     }
-
 }
+
