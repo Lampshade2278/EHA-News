23b20b85456eb15132acfe25b30fad8619774540
Hal <hjbieniek2003@gmail.com>
2024-11-30 20:15:58 -0600

    Re-add home controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EHA_News.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Index", "Article");
+        }
+    }
+}
