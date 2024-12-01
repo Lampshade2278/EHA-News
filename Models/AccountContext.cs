1ac1300ec35d4aeef8ef11d26a297063aae9f3db
Hal <hjbieniek2003@gmail.com>
2024-11-30 23:36:16 -0600

    Add DbContexts and libraries

---
+++ Models/AccountContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EHA_News.Models
+{
+    public class AccountContext : DbContext
+    {
+        public AccountContext(DbContextOptions<AccountContext> options) : base(options)
+            { }
+
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>().HasData(
+                new Account
+                {
+                    AccountId = "DoeJohn@fake.com",
+                    Password = "Te5tP@sswd",
+                    ConfirmPassword = "Te5tP@sswd",
+                    FirstName = "John",
+                    LastName = "Doe"
+                }
+            );
+        }
+    }
+}
---
+++ Models/ArticleContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EHA_News.Models
+{
+    public class ArticleContext : DbContext
+    {
+        public ArticleContext(DbContextOptions<ArticleContext> options) : base(options)
+        { }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category{CategoryId = "F", CategoryName = "Finance"},
+                new Category { CategoryId = "C", CategoryName = "Computing" },
+                new Category { CategoryId = "P", CategoryName = "Politics" },
+                new Category { CategoryId = "E", CategoryName = "Entertainment" }
+
+
+            );
+
+            modelBuilder.Entity<Article>().HasData(
+                new Article
+                {
+                    ArticleId = 1,
+                    Author = "John Doe II",
+                    Synopsis = "This is a test synopsis, and therefore is not at all a good read.",
+                    Headline = "Man creates Database in asp.NET Server Core 2022. Earns $1 Million",
+                    CategoryId = "C"
+                }
+            );
+        }
+    }
+}
