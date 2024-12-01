3eaae7f5c0f7802b4249de3e2f88f6c94d230971
E N <noralel053@gmail.com>
2024-11-30 22:57:11 -0600

    Adding more to the migrations

--- Migration/ArticleContextInfo.cs
+++ Migration/ArticleContextInfo.cs
@@ -1,4 +1,11 @@
-﻿namespace EHA_News.Migration
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using MovieList.Models;
+
+namespace EHA_News.Migration
 {
     public class ArticleContextInfo
     {
--- Migration/CatergoryContextInfo.cs
+++ Migration/CatergoryContextInfo.cs
@@ -1,4 +1,11 @@
-﻿namespace EHA_News.Migration
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+
+namespace EHA_News.Migration
 {
     public class CatergoryContextInfo
     {
