2ddcdfc3a380ef138a6cc37d94ad77e04c100c0b
E N <noralel053@gmail.com>
2024-12-01 19:48:40 -0600

    Fixing on the migration contexts

--- Migration/AccountContextInfo.cs
+++ Migration/AccountContextInfo.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using EHA_News.Models;
 
 namespace EHA_News.Migration
 {
-    public class AccountContextInfo
+    public class AccountContextInfo : DbContext
     {
+        public AccountContextInfo(DbContextOptions<AccountContextInfo> options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
     }
 }
--- Migration/ArticleContextInfo.cs
+++ Migration/ArticleContextInfo.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using EHA_News.Models;
 
 namespace EHA_News.Migration
 {
-    public class ArticleContextInfo
+    public class ArticleContextInfo : DbContext
     {
+        public ArticleContextInfo(DbContextOptions<ArticleContextInfo> options) : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
     }
 }
